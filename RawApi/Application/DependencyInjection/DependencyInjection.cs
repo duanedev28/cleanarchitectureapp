f829a7fbc63c1d1e09cc175a6fb78cd3cb51a490
duanehowell28 <dbasister@pangga.ph>
2025-10-04 13:18:21 +0800

    add global handlers

---
+++ RawApi/Application/DependencyInjection/DependencyInjection.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using RawApi.Application.Interfaces;
+using RawApi.Domain.Entities;
+using RawApi.Infra.DependencyService.Services.Auth;
+using RawApi.Infra;
+
+namespace RawApi.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services,
+         IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("Default")));
+
+            //Services
+            services.AddScoped<IProductService, ProductService>();
+            services.AddScoped<IAuthService, AuthService>();
+            services.AddSingleton<IJwtService, JwtService>();
+            services.AddSingleton<IPasswordHasher<User>, PasswordHasher<User>>();
+
+            return services;
+        }
+    }
+}
---
+++ RawApi/Infra/Exceptions/GlobalExceptionHandler.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RawApi.Infra.Exceptions
+{
+    internal sealed class GlobalExceptionHandler(RequestDelegate next,
+        ILogger<GlobalExceptionHandler> logger)
+    {
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Unhandled exception occurred!");
+
+                context.Response.StatusCode = ex switch
+                {
+                    ApplicationException => StatusCodes.Status400BadRequest,
+                    _ => StatusCodes.Status500InternalServerError
+                };
+
+                await context.Response.WriteAsJsonAsync(
+                    new ProblemDetails
+                    {
+                        Type = ex.GetType().Name,
+                        Title = "An error occurred",
+                        Detail = ex.Message,
+                    });
+            }
+        }
+    }
+}
